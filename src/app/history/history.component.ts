cc9eaf0c7e4add310003ab9dedce234a88f25c00
PabloAcker <favw14@gmail.com>
2024-11-12 18:04:13 -0400

    commit with history and llajta component edited with hover and slice moving

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-history',
@@ -8,13 +8,24 @@ import {CommonModule} from '@angular/common';
   templateUrl: './history.component.html',
   styleUrl: './history.component.css'
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
   images = [
-    '/assets/fondo-morado.jpg', // Reemplaza con la ruta de tus imágenes
+    '/assets/fondo-morado.jpg',
     '/assets/fondo-samurai.jpg',
     '/assets/Senna-League-Of-Legends-Reveal.png'
   ];
   currentIndex = 0;
+  intervalId: any;
+
+  ngOnInit(): void {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide(): void {
+    this.intervalId = setInterval(() => {
+      this.goNext();
+    }, 3000); // Cambia cada 3 segundos
+  }
 
   goNext(): void {
     this.currentIndex = (this.currentIndex + 1) % this.images.length;
@@ -26,5 +37,7 @@ export class HistoryComponent {
 
   setIndex(index: number): void {
     this.currentIndex = index;
+    clearInterval(this.intervalId); // Detiene el cambio automático al seleccionar manualmente
+    this.startAutoSlide(); // Reinicia el cambio automático
   }
 }
